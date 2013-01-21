9bfb80d5828707680edcd58bd7924e64126172d3
David Leatherman <david@leathekd.com>
2013-01-21 10:20:01 -0500

    Fixup project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject slog "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An experiment in logging Clojure maps rather than unparsable strings"
+  :url "https://github.com/leathekd/slog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[sonian/carica "1.0.2"]
                  [clj-stacktrace "0.2.5"]
                  [clojurewerkz/elastisch "1.0.2"]
-                 [org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[log4j "1.2.17"]
-                                  [org.clojure/tools.logging "0.2.3"]]}})
+                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.3"]]
+  :profiles {:dev {:dependencies [[log4j "1.2.17"]]}})
