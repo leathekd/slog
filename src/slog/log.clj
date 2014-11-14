d27c511bd526c2832d4dddf0d89878b464e41b03
David Leatherman <git@david.leatherman.fm>
2014-11-13 21:58:14 -0500

    removing some code

--- src/slog/log.clj
+++
@@ -1,8 +0,0 @@
-(ns slog.log
-  (:require [clojure.tools.logging :as log]
-            [slog.core :refer [log log-map Sloggable]]))
-
-(defrecord Logger []
-  Sloggable
-  (log [context log-map]
-    (log/log (:level log-map) (pr-str log-map))))
--- test/config.clj
+++
@@ -1,9 +0,0 @@
-{:slog
- {:loggers [] #_"can be one of :es, :log, or a seq of both"
-  :es {:connection-url "http://localhost:9200"
-       :index "slog"
-       :request-options
-       #_"options to be merged into the clj-http request options for
-          every request"
-       {:debug false
-        :basic-auth ["user" "Passw0rd"]}}}}
